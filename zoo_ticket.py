a5d496d880ec874d36f97ff9bc6ebae13612d71b
Adri Hilmie Mohd. Radzi <75724080+AdriHilmie29@users.noreply.github.com>
2021-01-08 23:22:02 +0800

    Update and rename zoo_ticket to zoo_ticket.py

--- zoo_ticket.py
+++ zoo_ticket.py
@@ -1,7 +1,7 @@
 #A particular zoo determines the price of admission based on the age of the guest. Guests 2 years of age and less are admitted without charge. Children between 3 and 12 years of
-age cost RM14.00. Seniors aged 65 years and over cost RM18.00. Admission for all other guests is RM23.00. Create a program that begins by reading the ages of all of the guests
-in a group from the user, with one age entered on each line. The user will enter a blank line to indicate that there are no more guests in the group. Then your program should
-display the admission cost for the group with an appropriate message. The cost should be displayed using two decimal places. 
+#age cost RM14.00. Seniors aged 65 years and over cost RM18.00. Admission for all other guests is RM23.00. Create a program that begins by reading the ages of all of the guests
+#in a group from the user, with one age entered on each line. The user will enter a blank line to indicate that there are no more guests in the group. Then your program should
+#display the admission cost for the group with an appropriate message. The cost should be displayed using two decimal places. 
 
 
 age=[]
